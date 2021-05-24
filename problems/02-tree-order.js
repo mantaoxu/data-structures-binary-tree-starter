be3716a392ae227661a0457252d0138007216623
username <mantaoxu2@gmail.com>
2021-05-24 15:18:30 -0700

    on problem 2

--- problems/02-tree-order.js
+++ problems/02-tree-order.js
@@ -1,13 +1,21 @@
 // Implement a function that takes the root of a Binary Tree
 // and returns an array containing the values from a pre-order
 // traversal.
+// self, left, right
 function preOrderArray(root) {
-    // Your code here
+    // let arr = [];
+    // if(!arr.length) return arr;
+    if(!root) {
+        return [];
+    }
+    return [root.value, ...root.left, ...root.right];
+
 }
 
 // Implement a function that takes the root of a Binary Tree
 // and returns an array containing the values from an in-order
 // traversal.
+// left, self, right
 function inOrderArray(root) {
     // Your code here
 }
@@ -15,6 +23,7 @@ function inOrderArray(root) {
 // Implement a function that takes the root of a Binary Tree
 // and returns an array containing the values from a post-order
 // traversal.
+//left, right, self
 function postOrderArray(root) {
     // Your code here
 }
