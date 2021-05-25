9bf7056d86a84674fd906f3f41a21e51122f2487
username <mantaoxu2@gmail.com>
2021-05-25 10:18:05 -0700

    finished the leecode problem

--- problems/04-bfs.js
+++ problems/04-bfs.js
@@ -2,13 +2,37 @@
 // binary tree, traverses it in depth-first-search
 // order, and returns an array containing the values
 // in the order you visited them.
+
+const { TreeNode } = require("./01-tree-node");
+
 // queue //first in first out
 function bfs(root){
-    // Your code here
-    if(!root) return [];
+//if there is no root, return []
+if (!this.root) {
+    return [];
+    }
+    //start a new Queue
+    const queue = new TreeNode();
+    //keep a tally of all values in the tree
+    const treeValues = [];
+    //add root to queue
+    queue.enqueue(this.root);
+    //while queue is not empty
+    while (queue.val !== 0) {
+    //get TreeNode Children
+    const nodeChildren = queue.left.val;
+    //if node has children, loop and add each to queue
+    if (nodeChildren.length !== 0) {
+        nodeChildren.forEach(child => queue.enqueue(child));
+    }
+    //push the first item in the queue to the tree values
+    treeValues.push(queue.left.val);
+    //remove first node from queue
+    queue.dequeue();
+    }
+    //return values, should be all TreeNodes
+    return treeValues;
 
-    const finalArr = [];
-    
 
 
 
--- problems/05-leet-code-105.js
+++ problems/05-leet-code-105.js
@@ -4,5 +4,28 @@
 const { TreeNode } = require('./01_tree_node.js');
 
 function buildTree(preorder, inorder) {
+  if (preorder.length === 0 && inorder.length === 0) return null;
+  /**located the root value */
+  let rootVal = preorder[0]; // always the first number as the root;
+  let root = new TreeNode(rootVal);  //create a node base on the rootVal / using the value;
+
+  let midIdx = inorder.indexOf(rootVal);  //find the index of rootVal in inorder array, called this middle point(midIdx)
+  //**split the inorder array*//////
+  let leftInorder = inorder.slice(0, midIdx);    //everything found in the left, should go to the left sub-tree;
+  let rightInorder = inorder.slice(midIdx + 1); //everything found in the right, should go to the right sub-tree;
+
+  // check if inorder array's value if also part of the preorder array value//
+  let leftPreorder = preorder.filter((val) => leftInorder.includes(val));
+  let rightPreorder = preorder.filter((val) => rightInorder.includes(val));
+
+    /**build a recursive call */
+  let leftTree = buildTree(leftPreorder, leftInorder);
+  let rightTree = buildTree(rightPreorder, rightInorder);
+
+  /**build a sub-tree */
+  root.left = leftTree;
+  root.right = rightTree;
+  
+  return root;
 
 }
